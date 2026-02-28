d76747ee0996137bb6a66c1517bc96f3c296b6c7
Huang <2227027947@qq.com>
2026-02-28 18:19:52 +0800

    feat: add @Version to Phone

--- spring-old-phone-deals/src/main/java/com/oldphonedeals/entity/Phone.java
+++ spring-old-phone-deals/src/main/java/com/oldphonedeals/entity/Phone.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.Version;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -24,6 +25,9 @@ public class Phone {
     
     @Id
     private String id;
+
+    @Version
+    private Long version;
     
     private String title;
     
